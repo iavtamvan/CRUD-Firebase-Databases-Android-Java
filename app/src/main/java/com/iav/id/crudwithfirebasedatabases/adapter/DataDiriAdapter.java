96369ab4ecc0ac40f14f19b118d10d16976bb19d
root <ade.fajr.ariav@gmail.com>
2018-08-24 22:48:55 +0700

    Update not Fixed force close GetKey#2

--- app/src/main/java/com/iav/id/crudwithfirebasedatabases/adapter/DataDiriAdapter.java
+++ app/src/main/java/com/iav/id/crudwithfirebasedatabases/adapter/DataDiriAdapter.java
@@ -1,14 +1,20 @@
 package com.iav.id.crudwithfirebasedatabases.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.iav.id.crudwithfirebasedatabases.R;
 import com.iav.id.crudwithfirebasedatabases.model.DataDiriModel;
 
@@ -17,6 +23,8 @@ import java.util.ArrayList;
 public class DataDiriAdapter extends RecyclerView.Adapter<DataDiriAdapter.ViewHolder> {
     private ArrayList<DataDiriModel> dataDiriModels;
     private Context context;
+
+    private DatabaseReference databaseReference;
 
     public DataDiriAdapter(ArrayList<DataDiriModel> dataDiriModels, Context context) {
         this.dataDiriModels = dataDiriModels;
@@ -41,7 +49,36 @@ public class DataDiriAdapter extends RecyclerView.Adapter<DataDiriAdapter.ViewHo
         cvklik.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(context, "" + tvName.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+                // deelete
+            }
+        });
+
+        cvklik.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                DataDiriModel dataDiriModel = null;
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog_rfit);
+                dialog.setTitle("Pilih Aksi");
+                Button btnEdit = dialog.findViewById(R.id.btn_edit);
+                EditText edtEditNama = dialog.findViewById(R.id.edt_nama_edit);
+                EditText edtEditAlamat = dialog.findViewById(R.id.edt_alamat_edit);
+
+
+                databaseReference = FirebaseDatabase.getInstance().getReference();
+                databaseReference.child("barang")// akses parrent nya paling ataas
+                .child(dataDiriModel.getKey())
+                        .setValue(new DataDiriModel(edtEditNama.getText().toString().trim(), edtEditAlamat.getText().toString().trim()))
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                            }
+                        });
+
+                dialog.show();
+
+                return true;
             }
         });
 
